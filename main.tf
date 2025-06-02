71771d4a5fa20c2ccf47bd902da9b954cc5ed9c5
Namratha <namratha343@gmail.com>
2025-06-02 07:41:58 +0530

    map updated

--- main.tf
+++ main.tf
@@ -10,18 +10,18 @@ resource "aws_vpc" "myvpc" {
 }
 
 resource "aws_subnet" "sub1" {
-  vpc_id                          = aws_vpc.myvpc.id
-  cidr_block                      = "10.0.0.0/24"
-  availability_zone               = "ap-south-1a"
-  map_customer_owned_ip_on_launch = true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
 
 }
 
 resource "aws_subnet" "sub2" {
-  vpc_id                          = aws_vpc.myvpc.id
-  cidr_block                      = "10.0.1.0/24"
-  availability_zone               = "ap-south-1b"
-  map_customer_owned_ip_on_launch = true
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
 
 }
 
